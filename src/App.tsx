ae723405121af72c3cd987d763cebe4707d33544
eddyzera <edgar.nascimento@newgo.com.br>
2022-10-04 20:14:56 -0300

    [feat]: use global style and definition the font

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export const App: React.FunctionComponent = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>hello guys</h1>
     </ThemeProvider>
   )
