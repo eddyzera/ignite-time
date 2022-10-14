c892f2c14302fb0a9d6520d3a110d87fdc984728
eddyzera <edgar.nascimento@newgo.com.br>
2022-10-14 17:11:34 -0300

    [refactor]: change style of component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { HeaderContainer } from './styles'
+import LogoIgnite from '../../assets/logo-ignite.svg'
+import { Timer, Scroll } from 'phosphor-react'
 
 export const Header: React.FunctionComponent = () => {
   return (
     <HeaderContainer>
-      <span>logo</span>
+      <img src={LogoIgnite} alt="Dois triangulos colocados" />
       <nav>
-        <a href="#">timer</a>
-        <a href="#">history</a>
+        <NavLink to="/" end title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
