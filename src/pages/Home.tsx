b9137c890608b612ba9742041ee4061669ae253f
eddyzera <edgar.nascimento@newgo.com.br>
2022-10-17 17:44:19 -0300

    [feat]: created page home

--- src/pages/Home.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const Home: React.FunctionComponent = () => {
-  return <h1>hello home</h1>
-}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,36 @@
+import { Play } from 'phosphor-react'
+import React from 'react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export const Home: React.FunctionComponent = () => {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+          <span>minutos .</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <button>
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
