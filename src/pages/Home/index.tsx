0fc0878d11f9ff2e963e439835158f93d4cf22ad
eddyzera <edgar.nascimento@newgo.com.br>
2022-10-19 19:16:54 -0300

    [refactor]: reset value when submit form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,7 +24,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export const Home: React.FunctionComponent = () => {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -33,6 +33,7 @@ export const Home: React.FunctionComponent = () => {
   })
   const handleCreateNewCicle = (data: any) => {
     console.log(data)
+    reset()
   }
   const task = watch('task')
   const isSubmitDisabled = !task
